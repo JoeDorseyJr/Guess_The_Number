8ed90a29849deb550a2470b9927b1be87c49efd6
Joe Dorsey <joedorseyjr@gmail.com>
2021-11-20 09:40:50 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner userInput = new Scanner(System.in);
+    public static int from = 1;
+    public static int to = 20;
+
+    public static void main(String[] args) {
+
+        String name="";
+        boolean play = true;
+
+        try {
+            System.out.println("Hi! What's your name?");
+            name = userInput.nextLine();
+            do {
+                guessTheNumber(name,from,to);
+                play = playAgain();
+            } while (play);
+
+        } catch (InterruptedException e) {
+            userInput.nextLine();
+        }
+        System.out.println("Good-bye!");
+    }
+
+    public static void guessTheNumber(String name,int from,int to) throws InterruptedException {
+        int attemptsAllowed = 5;
+        int attempted = 0;
+        int guess = 0;
+        boolean win = false;
+        int number = randInt(from,to);
+        int delay=1500;
+        String attempt ="";
+        String winMessage="";
+        String[] messages = {"Well, %s, I am thinking of a number between %d and %d.","Can you guess my number?","Take a guess."};
+        for (String message : messages) {
+            System.out.printf(message+"\n",name,from,to);
+            Thread.sleep(delay);
+            delay -=500;
+        }
+        while(!win && attemptsAllowed != attempted) {
+            try {
+                guess = userInput.nextInt();
+                win = guess == number;
+                if (guess > number) {
+                    System.out.println("You're too high!");
+                } else if (guess < number) {
+                    System.out.println("You're too low!");
+                }
+                attempted++;
+                if (attempted != attemptsAllowed && !win){
+                    System.out.printf(messages[2] + " %d %s left.\n",Math.abs(attempted-attemptsAllowed),attempt = Math.abs(attempted-attemptsAllowed) > 1 ? "guesses": "guess");
+                }
+            } catch (Exception e) {
+                String wrongInput = userInput.nextLine();
+                System.err.printf("Wrong input type. '%s' is not a number.\n",wrongInput);
+            }
+        }
+
+        if (win) {
+            winMessage = win && attempted == 1 ? "And with your FIRST GUESS!":"You guessed my number in %d guesses!";
+            System.out.printf("%s, You Won! " + winMessage +"\n",name,attempted);
+        } else {
+            System.out.println("You lose. You ran out of guesses.");
+        }
+    }
+
+    public static int randInt(int start, int stop){
+        if (start < 0 ) {
+            return (int) Math.round(Math.random() * (Math.abs(start - stop)) + start);
+        }
+        return (int) Math.round(Math.random() * stop + start);
+    }
+
+    public static boolean playAgain(){
+        String ans ="";
+        while (!ans.equalsIgnoreCase("y") && !ans.equalsIgnoreCase("n")) {
+            System.out.println("Would you like to play again? (Y or N)");
+            ans = userInput.next();
+        }
+        return ans.equalsIgnoreCase("y");
+    }
+}
